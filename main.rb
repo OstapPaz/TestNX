b6ab8de4bd5bdcf883bf93e25b75b7a5dad66bb4
OstapPaz <pazynuyk.ostap@gmail.com>
2018-10-03 23:44:38 +0300

    C:/Program Files/Git

--- main.rb
+++ main.rb
@@ -1,42 +1,46 @@
 class QuestionMarkChecker
-  def questions_marks(str)
- str = str.gsub(/[a-zA-Z]/, '')
- found = []
-
-     str.each_char.with_index do |char, idx|
-       if char =~ /\d/
-         if str[idx + 1] =~ /\d/
-           if char.to_i + str[idx + 1].to_i == 10
-             return false
-           end
-         elsif str[idx + 2] =~ /\d/
-           if char.to_i + str[idx + 2].to_i == 10
-             return false
-           end
-         elsif str[idx + 3] =~ /\d/
-           if char.to_i + str[idx + 3].to_i == 10
-             return false
-           end
-         elsif str[idx + 4] =~ /\d/
-           if char.to_i + str[idx + 4].to_i == 10
-             if str[idx + 1] == '?' && str[idx + 2] == '?' && str[idx + 3] == '?'
-               found << true
-             end
-           end
-         end
-       end
-     end
-
-     if found.empty?
-       false
-     else
-       true
-     end
-
+  def initialize(string)
+    @string = string
   end
 
+  def check
+    found = []
 
+    decorated_string.each_char.with_index do |char, idx|
+      if char =~ /\d/
+        if decorated_string[idx + 1] =~ /\d/
+          if char.to_i + decorated_string[idx + 1].to_i == 10
+            return false
+          end
+        elsif decorated_string[idx + 2] =~ /\d/
+          if char.to_i + decorated_string[idx + 2].to_i == 10
+            return false
+          end
+        elsif decorated_string[idx + 3] =~ /\d/
+          if char.to_i + decorated_string[idx + 3].to_i == 10
+            return false
+          end
+        elsif decorated_string[idx + 4] =~ /\d/
+          if char.to_i + decorated_string[idx + 4].to_i == 10
+            if decorated_string[idx + 1] == '?' && decorated_string[idx + 2] == '?' && decorated_string[idx + 3] == '?'
+              found << true
+            end
+          end
+        end
+      end
+    end
+
+    found.any?
+  end
+
+  private
+
+  attr_reader :string
+
+  def decorated_string
+    @decorated_string ||= string.gsub(/[a-zA-Z]/, '')
+  end
 end
 
- puts QuestionMarkChecker.new.questions_marks('as???as???da???')
+puts QuestionMarkChecker.new('as???as???da???').check
 
